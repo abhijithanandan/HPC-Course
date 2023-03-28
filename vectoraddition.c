1ea12d3308c62734c52af033d62488a5a1b6a899
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-03-28 13:03:04 +0530

    Added a simple script to compute the walltime of OpenMP vector addition

---
+++ vectoraddition.c
@@ -0,0 +1,47 @@
+// A simple script to do vector addition with openmp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main (int argc, char *argv[])
+{
+  int n, i;
+
+  double *x, *y;
+
+// Get input array size
+  
+  n = atoi(argv[1]);
+
+  x = (double *) malloc(n*sizeof(double));
+
+  y = (double *) malloc(n*sizeof(double));
+
+// Initialize x and y arrays
+
+  for (i = 0; i < n; i++) {
+      x[i] = 1.0;
+      y[i] = 2.0;
+    }
+
+  // Compute the wall time
+
+  double start = omp_get_wtime();
+
+  #pragma omp parallel for private(i) shared(x,y)
+  for (i = 0; i < n; i++) {
+      x[i] = x[i] + y[i];
+    }
+
+  double end = omp_get_wtime();
+
+  printf("Time taken: %f seconds \n", end - start);
+
+  free(x);
+  free(y);
+
+  printf("Done!");
+
+  return 0;
+}
