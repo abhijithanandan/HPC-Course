fae3a51bbf8edfbc40271a47aa89a83da39d19da
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-03-31 22:14:03 +0530

    Added examples for signals

---
+++ singal.c
@@ -0,0 +1,21 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// alter the behavior of the process when a sinal is received is raised
+void sigint_handler(int signo) {
+  write(STDOUT_FILENO, "I won't die!\n", signo);
+}
+
+int main() {
+  // run the handler whenerver SIGINT is raised
+  signal(SIGINT, sigint_handler);
+  // run the handler whenerver SIGTERM is raised
+  signal(SIGTERM, sigint_handler);
+  while (1) {
+    printf("This process with pid %d will run forever.\n", getpid());
+    sleep(1);
+  }
+  return 0;
+}
