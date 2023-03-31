4304a9580b9c31f7aa319caac2c283df9ad34c7f
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-03-31 23:13:55 +0530

    Added minor typo correction

--- singal.c
+++ singal.c
@@ -4,15 +4,15 @@
 #include <unistd.h>
 
 // alter the behavior of the process when a sinal is received is raised
-void sigint_handler(int signo) {
+void handler(int signo) {
   write(STDOUT_FILENO, "I won't die!\n", signo);
 }
 
 int main() {
   // run the handler whenerver SIGINT is raised
-  signal(SIGINT, sigint_handler);
+  signal(SIGINT, handler);
   // run the handler whenerver SIGTERM is raised
-  signal(SIGTERM, sigint_handler);
+  signal(SIGTERM, handler);
   while (1) {
     printf("This process with pid %d will run forever.\n", getpid());
     sleep(1);
